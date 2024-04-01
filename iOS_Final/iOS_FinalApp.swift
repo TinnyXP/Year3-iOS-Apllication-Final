1d9030fe94574a2a1bd7c5436469a5f05a124769
Trin Pongsri✌ <trin.pongsri@gmail.com>
2024-04-01 20:37:24 +0700

    Initial Commit

---
+++ iOS_Final/iOS_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_FinalApp.swift
+//  iOS_Final
+//
+//  Created by Trin Pongsri on 1/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
